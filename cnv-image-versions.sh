706725abeb6283f187d8a8838704d9ccef0b2b9d
Ofir Nash <58515394+ofirnash1@users.noreply.github.com>
2021-01-16 16:54:47 +0200

    Fix coding styling in cnv-image-versions script

--- cnv-image-versions.sh
+++ cnv-image-versions.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-YELLOW=`tput setaf 3`
-RESET=`tput sgr0`
+YELLOW=$(tput setaf 3)
+RESET=$(tput sgr0)
+OCP_CNV="openshift-cnv"
 
 RESOURCE_OPTIONS=("daemonset" "deployment")
 
@@ -9,33 +10,33 @@ RESOURCE_OPTIONS=("daemonset" "deployment")
 echo "Select from which resource:"
 select res_opt in "${RESOURCE_OPTIONS[@]}"
 do
-	echo "Selected option: ${YELLOW}$res_opt${RESET}."
+  echo "Selected option: ${YELLOW}$res_opt${RESET}."
 
-	cnv_image_choices="oc get ${res_opt} -n openshift-cnv | awk ' { print \$1 } ' | tail -n +2"
-	cnv_image_choices=`eval "$cnv_image_choices"`
+  cnv_image_choices="oc get ${res_opt} -n ${OCP_CNV} | awk ' { print \$1 } ' | tail -n +2"
+  cnv_image_choices=$(eval "$cnv_image_choices")
 
-	choices_array=( $cnv_image_choices )
+  choices_array=( ${cnv_image_choices} )
 
-	# Second Menu - Which image in the resource
-	echo "Select the image you would like to check:"
-	select opt in "${choices_array[@]}"
-	do
-	    echo "Selected option: ${YELLOW}$opt${RESET}. Extracting version..."
+  # Second Menu - Which image in the resource
+  echo "Select the image you would like to check:"
+  select opt in "${choices_array[@]}"
+  do
+    echo "Selected option: ${YELLOW}$opt${RESET}. Extracting version..."
 
-	    image="oc get ${res_opt} -n openshift-cnv ${opt} -oyaml | awk '/image:/' | tail -1"
-	    image=`eval "$image"`
+    image="oc get ${res_opt} -n ${OCP_CNV} ${opt} -oyaml | awk '/image:/' | tail -1"
+    image=$(eval "$image")
 
-		# Extract container's sha256
-		container_sha=${image##*container-native-virtualization/}
+    # Extract container's sha256
+    container_sha=${image##*container-native-virtualization/}
 
-		# Concatenate docker path and sha256 value
-		docker_img="docker://registry-proxy.engineering.redhat.com/rh-osbs/container-native-virtualization-${container_sha}"
+    # Concatenate docker path and sha256 value
+    docker_img="docker://registry-proxy.engineering.redhat.com/rh-osbs/container-native-virtualization-${container_sha}"
 
-		extract_from_proxy="skopeo inspect '${docker_img}'"
+    extract_from_proxy="skopeo inspect '${docker_img}'"
 
-		# Get version
-		version_num=`eval "${extract_from_proxy} | jq -r '.Labels.version','.Labels.release' |xargs | sed 's/ /-/g'"`
+    # Get version
+    version_num=$(eval "${extract_from_proxy} | jq -r '.Labels.version','.Labels.release' |xargs | sed 's/ /-/g'")
 
-		echo "${YELLOW}$opt ${RESET}version is: ${YELLOW}${version_num}${RESET}"
-	done
+    echo "${YELLOW}$opt ${RESET}version is: ${YELLOW}${version_num}${RESET}"
+  done
 done
